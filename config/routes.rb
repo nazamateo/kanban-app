96957aabf44928a2915fec36e21474c754e92478
nazamateo <nazamateo@gmail.com>
2022-06-22 18:59:38 +0800

    done with dashboard design for initial presentation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/calendar', to: 'calendar#index', as: 'calendar'
-  root to: "home#index"
+  root to: "categories#index"
   resources :categories do
     resources :tasks, shallow: true
     end
